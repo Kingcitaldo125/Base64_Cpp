8c5c0dd3292880514c9a0b77cd87abfc802fc3e6
Paul Arelt <paularelt@gmail.com>
2021-07-23 17:30:08 -0400

    print real along with actual

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,13 @@ using std::endl;
 int main()
 {
   // Simply using the Thomas Hobbes's Leviathan example from Wikipedia to do basic testing
-  cout << base64encode("M") << endl; // TQ==
-  cout << base64encode("Ma") << endl; // TWE=
-  cout << base64encode("Man") << endl; // TWFu
-  cout << base64encode("Man ") << endl; // TWFuIA==
-  cout << base64encode("Man i") << endl; // TWFuIGk=
-  cout << base64encode("Man is") << endl; // TWFuIGlz
+  cout << "TQ== : " << base64encode("M") << endl;
+  cout << "TWE= : " << base64encode("Ma") << endl;
+  cout << "TWFu : " << base64encode("Man") << endl;
+  cout << "TWFuIA== : " << base64encode("Man ") << endl;
+  cout << "TWFuIGk= : " << base64encode("Man i") << endl;
+  cout << "TWFuIGlz : " << base64encode("Man is") << endl;
 
   return 0;
 }
+
